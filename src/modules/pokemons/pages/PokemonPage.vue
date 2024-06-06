bf808cf867b9d08aeba033e49a0cce1edb85de7f
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-06-05 21:04:23 -0500

    Set pokemon props

---
+++ src/modules/pokemons/pages/PokemonPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  id: number
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <section class="flex flex-col">
+    <h1 class="text-3xl font-bold">
+      Pokémon <small class="text-blue-500">#{{ id }}</small>
+    </h1>
+    <img
+      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`"
+      alt="pokemon image"
+      class="size-52 mt-5"
+    />
+    <RouterLink
+      :to="{ name: 'pokemon', params: { id: id + 1 } }"
+      class="bg-blue-500 text-white p-2 rounded mt-2 text-center hover:bg-blue-700"
+      >Siguiente</RouterLink
+    >
+  </section>
+</template>
